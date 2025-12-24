ef98c570bf69f679c0fe387f5ac1b5001e329ab2
maingocduy11 <2051063514@e.tlu.edu.vn>
2025-12-24 12:33:11 +0700

    commit

--- book.js
+++ book.js
@@ -2,8 +2,9 @@
 // Mặc định nếu backend không trả dữ liệu
 const defaultMessageData = [
   "Giáng sinh này,",
-  "mong bạn luôn bình an,",
-  "ấm áp bên những người mình thương.",
+  "chúc chị luôn bình an,",
+  "ấm áp bên gia đình và người thân,",
+  "và thật nhiều niềm vui trong mùa đông này.",
 ];
 
 // Lấy dữ liệu bức thư từ API (trung thu / noel) nếu có
